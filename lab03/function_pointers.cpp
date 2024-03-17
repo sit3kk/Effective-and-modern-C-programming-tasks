e794ba7c2f5a9132a5ba8b9c2a5fb2f8ce2105da
sit3kk <sito.270@interia.pl>
2024-03-17 19:12:30 +0100

    Ex 01

--- lab03/function_pointers.cpp
+++ lab03/function_pointers.cpp
@@ -3,20 +3,24 @@
 using namespace std;
 /// Computes n-th root of x
 double sqrtn(int n, double x){
-	return exp( log(x) / n); 
+    return exp( log(x) / n); 
 }
 double power(double  x, double y){
-	return exp( log(x) * y);
+    return exp( log(x) * y);
 }
 template <typename F, typename G>
 double function(int n, double y, double z, F f, G g){
-	return (f(n,y) > z) ? g(z, y) : g(y, z);
+    return (f(n,y) > z) ? g(z, y) : g(y, z);
 }
+
 int main(){
-	auto fp1 = sqrtn;
-    auto fp2 = power;
-	auto fp3 = function<decltype(fp1), decltype(fp2)>;
-	cout << fp3(2, 10, 3, fp1, fp2) << endl;
-	cout << fp3(3, 10, 3, fp1, fp2) << endl;
-	return 0;
+ 
+    double (*fp1)(int, double) = sqrtn;
+    double (*fp2)(double, double) = power;
+  
+    double (*fp3)(int, double, double, double (*)(int, double), double (*)(double, double)) = function;
+    
+    cout << fp3(2, 10, 3, fp1, fp2) << endl;
+    cout << fp3(3, 10, 3, fp1, fp2) << endl;
+    return 0;
 }
