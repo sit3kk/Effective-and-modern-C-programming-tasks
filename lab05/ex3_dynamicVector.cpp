7107222fa014ac67d3712088eaedb9621fd5593b
sit3kk <sito.270@interia.pl>
2024-04-04 14:29:41 +0200

    ex3  correction

--- lab05/ex3_dynamicVector.cpp
+++ lab05/ex3_dynamicVector.cpp
@@ -5,38 +5,43 @@ using namespace std;
 
 #define LOG(x) std::cout << #x << " = " << (x) << std::endl
 
-int main(){
-    cout <<fixed << setprecision(2);
+int main()
+{
+    cout << fixed << setprecision(2);
     using DWector = Vector<double, 0>;
 
-    DWector m0{4}; // Vector filled with zeroes
+    DWector m0(4); // Vector filled with zeroes
     LOG(m0);
 
-    DWector m1{1.,  2.,  3.,  4.};   // Vector of size 4 from initialization_list
+    DWector m1{1., 2., 3., 4.}; // Vector of size 4 from initialization_list
     LOG(m1);
 
-    const DWector m2 = m1;              // Copy constructor
-    m1[2] = -5;  m1[3] = -20;
-  	LOG(m1);
+    const DWector m2 = m1; // Copy constructor
+    m1[2] = -5;
+    m1[3] = -20;
+    LOG(m1);
 
-  	DWector::value_type element = m2[2];
-  	LOG(element);
-  	LOG(m2.size());
+    DWector::value_type element = m2[2];
+    LOG(element);
+    LOG(m2.size());
 
     DWector m3 = m1 + m2;
     LOG(m3);
 
-	m1.resize(5);
-	LOG(m1);
-	LOG(m1.size());
+    m1.resize(5);
+    LOG(m1);
+    LOG(m1.size());
 
     Vector<double, 0> m4{5.0, 6.2, 1.34};
-    try {
+    try
+    {
         auto mm = m3 + m4; // ERROR: exception
-    } catch(std::exception & e){
+    }
+    catch (std::exception &e)
+    {
         cout << " Exception : " << e.what() << endl;
     }
-	return 0;
+    return 0;
 }
 /** Expected output
 m0 = 0.00 0.00 0.00 0.00
