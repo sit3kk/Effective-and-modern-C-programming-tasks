0ec3ac7bfbd3c8a1636f545649242d1e4d8dcf33
sit3kk <sito.270@interia.pl>
2024-04-14 16:06:28 +0200

    Ex3 Done

--- lab06/ex3_Mixins.cpp
+++ lab06/ex3_Mixins.cpp
@@ -1,56 +1,79 @@
 #include <iostream>
 #include <vector>
+#include <utility>
 using namespace std;
 
-/* Colors */
-class Red{
-public:     static constexpr char color[] = "red";
+class Red
+{
+public:
+    static constexpr char color[] = "red";
 };
 
-class Yellow{
-public:     static constexpr char color[] = "yellow";
+class Yellow
+{
+public:
+    static constexpr char color[] = "yellow";
 };
 
-class Blue{
-public:     static constexpr char color[] = "blue";
+class Blue
+{
+public:
+    static constexpr char color[] = "blue";
 };
 
-/* Shapes*/
-class Circle{
-    double r=0;
+class Circle
+{
+    double r = 0;
+
 public:
     Circle() = default;
-    Circle(double r): r(r) {}
-    double area(){ return 3.24*r*r; }
+    Circle(double r) : r(r) {}
+    double area() const { return 3.14 * r * r; }
 };
-class Rectangle{
-    double a=0, b=0;
+
+class Rectangle
+{
+    double a = 0, b = 0;
+
 public:
     Rectangle() = default;
     Rectangle(double a, double b) : a(a), b(b) {}
-    double area(){ return a * b;}
+    double area() const { return a * b; }
 };
-/* Other Features */
-class Note{
-    std::vector<std::string> notes;
+
+class Note
+{
+    vector<string> notes;
+
 public:
-    Note(const std::string & note){
+    Note(const string &note)
+    {
         add(note);
     }
-    void add(const std::string & note){
+    void add(const string &note)
+    {
         notes.push_back(note);
     }
 };
 
-int main(){
+template <typename... Bases>
+class Mixins : public Bases...
+{
+public:
+    template <typename... Args>
+    Mixins(Args &&...args) : Bases(std::forward<Args>(args))... {}
+};
 
+int main()
+{
     using RedCircle = Mixins<Red, Circle>;
     RedCircle x(Red{}, Circle{3});
     cout << x.color << " area = " << x.area() << endl;
 
     using BlueRectangleWithNotes = Mixins<Blue, Rectangle, Note>;
-    RedCicleWithNotes y (Blue{}, Rectangle{3,4}, Note{"Hey"});
+    BlueRectangleWithNotes y(Blue{}, Rectangle{3, 4}, Note{"Hey"});
     y.add("Ho");
     cout << y.color << " area = " << y.area() << endl;
 
+    return 0;
 }
