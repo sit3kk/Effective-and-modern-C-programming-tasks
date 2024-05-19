695845b2dd0b71ed4c8b29ef8e5044b028614053
sit3kk <sito.270@interia.pl>
2024-05-19 16:34:22 +0200

    9.3 Done

--- lab09/ex_9_3_Trie.cpp
+++ lab09/ex_9_3_Trie.cpp
@@ -1,44 +1,134 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <map>
 using namespace std;
 /**
  * Trie structure
  *
  * HINT: use std::map to implement it!
- *       My implementation adds less than 25 lines of code. 
+ *       My implementation adds less than 25 lines of code.
  */
-class Trie{
-  /// TODO:
+class Trie
+{
+private:
+    struct TrieNode
+    {
+        map<string, TrieNode *> children;
+        bool isEndOfSentence;
+        TrieNode() : isEndOfSentence(false) {}
+    };
+
+    TrieNode *root;
+
+    void addHelper(TrieNode *node, const vector<string> &sentence, int index)
+    {
+        if (index == sentence.size())
+        {
+            node->isEndOfSentence = true;
+            return;
+        }
+        if (node->children.find(sentence[index]) == node->children.end())
+        {
+            node->children[sentence[index]] = new TrieNode();
+        }
+        addHelper(node->children[sentence[index]], sentence, index + 1);
+    }
+
+    void printEndingsHelper(TrieNode *node, vector<string> currentSentence)
+    {
+        if (node->isEndOfSentence)
+        {
+            printSentence(currentSentence);
+            cout << endl;
+        }
+        for (auto &child : node->children)
+        {
+            currentSentence.push_back(child.first);
+            printEndingsHelper(child.second, currentSentence);
+            currentSentence.pop_back();
+        }
+    }
+
+    TrieNode *findNode(TrieNode *node, const vector<string> &sentence, int index)
+    {
+        if (index == sentence.size())
+        {
+            return node;
+        }
+        if (node->children.find(sentence[index]) == node->children.end())
+        {
+            return nullptr;
+        }
+        return findNode(node->children[sentence[index]], sentence, index + 1);
+    }
+
+    void deleteTrie(TrieNode *node)
+    {
+        for (auto &child : node->children)
+        {
+            deleteTrie(child.second);
+        }
+        delete node;
+    }
+
 public:
-    static void printSentence(const std::vector<std::string>  & sentence ){
-        for(const auto & w : sentence)
+    Trie()
+    {
+        root = new TrieNode();
+    }
+
+    ~Trie()
+    {
+        deleteTrie(root);
+    }
+
+    static void printSentence(const vector<string> &sentence)
+    {
+        for (const auto &w : sentence)
             cout << w << " ";
     }
-    void add(const std::vector<std::string>  & sentece ){
-       cout << "Adding : ";
-       printSentence(sentece);
-       cout << "\n";
-       /// TODO:
+
+    void add(const vector<string> &sentence)
+    {
+        cout << "Adding: ";
+        printSentence(sentence);
+        cout << "\n";
+        addHelper(root, sentence, 0);
     }
-    void printPossibleEndings(const std::vector<std::string>  & beginningOfSentece ){
+
+    void printPossibleEndings(const vector<string> &beginningOfSentence)
+    {
         cout << "Endings for \"";
-        printSentence(beginningOfSentece);
-        cout << "\" are :\n";
-        // TODO:
+        printSentence(beginningOfSentence);
+        cout << "\" are:\n";
+        TrieNode *node = findNode(root, beginningOfSentence, 0);
+        if (node)
+        {
+            printEndingsHelper(node, beginningOfSentence);
+        }
+        else
+        {
+            cout << "No endings found.\n";
+        }
     }
-    void load(const std::string & fileName){
+
+    void load(const string &fileName)
+    {
         ifstream file(fileName);
-        if(!file){
-            cerr << "Error when openning file " << fileName << endl;
+        if (!file)
+        {
+            cerr << "Error opening file " << fileName << endl;
             return;
         }
         string word;
         vector<string> sentence;
-        while( file >> word) {
+        while (file >> word)
+        {
             sentence.push_back(word);
-            // is it end of the sentence?
-            if(word.find_last_of('.') != string::npos) {
+
+            if (word.find_last_of('.') != string::npos)
+            {
                 add(sentence);
                 sentence.clear();
             }
@@ -46,15 +136,15 @@ public:
     }
 };
 
-
-int main(){
+int main()
+{
     Trie dictionary;
     dictionary.load("../sample.txt");
-    //dictionary.load("hamletEN.txt");
+    // dictionary.load("hamletEN.txt");
 
     dictionary.printPossibleEndings({"Curiosity"});
     dictionary.printPossibleEndings({"Curiosity", "killed"});
-    dictionary.printPossibleEndings({"The", "mouse","was","killed"});
+    dictionary.printPossibleEndings({"The", "mouse", "was", "killed"});
 
     /* cout << "Beginning of sentence (ended with single .): ";
     string word;
